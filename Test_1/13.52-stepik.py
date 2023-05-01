9c49f74ef2f49d4a14d2ae65e1f4abf3b4c5d3f2
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-05-01 21:54:58 +0300

    Add

--- Test_1/13.52-stepik.py
+++ Test_1/13.52-stepik.py
@@ -6,8 +6,8 @@
 
 def is_prime(num):
     b = 0
-    for i in range(1, n + 1):
-        if n % i == 0:
+    for i in range(1, num + 1):
+        if num % i == 0:
             b = b + 1
     if b == 2:
         return True
--- Test_1/13.53-stepik.py
+++ Test_1/13.53-stepik.py
@@ -3,20 +3,13 @@
 натуральное число num и возвращает первое простое число большее числа num.
 Примечание 1. Используйте функцию is_prime() из предыдущей задачи.
 '''
-def is_prime(num):
-    # # flag = False
-    # b = 0
-    # for i in range(1, n + 1):
-    #     if n % i == 0:
-    #         b = b + 1
-    # return b == 2
-    # # if b == 2:
-    # #     flag = True
-    # # return flag   
-    return len([i for i in range(1, num+1) if num % i == 0]) == 2       
 
-# n = int(input())
-# print(is_prime(n))
+def is_prime(num):
+    b = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            b = b + 1
+    return b == 2
 
 def get_next_prime(num):
     c = n + 1
@@ -24,18 +17,11 @@ def get_next_prime(num):
         c = c + 1
     return c
 
-
-
-
-        
-    
-
 n = int(input())
-
 print(get_next_prime(n))
 
 
-
+# Вариат №2 без использования внутринней функции для проверки на простоту числа
 
 # def get_next_prime(num):
 #     b = 0
